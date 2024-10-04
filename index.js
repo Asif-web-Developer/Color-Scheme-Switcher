49a764aaec9fc118c7707d06392c9feaf4fb2c21
Asif-web-Developer <asifchowdury8998@gmail.com>
2024-10-04 23:05:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e)=>{
+        const color = e.target.id;
+        body.style.backgroundColor = color;
+        
+    })
+});
